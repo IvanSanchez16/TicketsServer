85bd8a57d542fb9e985f1ddf11181d9fe6e01592
Ivan Sanchez <ivanhumberto_16@hotmail.com>
2021-06-07 12:07:12 -0600

    Instalacion de cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 
@@ -6,6 +7,7 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
+app.use(cors());
 
 require('./config/connection');
 
